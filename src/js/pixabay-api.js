b6c740a6c72ae028be554068fd50cc9178746713
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-01-30 00:15:34 +0300

    add axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,21 @@
+import axios from 'axios';
 export const fetchByQuery = searchedQuery => {
-    return fetch(
-        `https://pixabay.com/api/?key=48282241-c94e9d668c7a92092d53abf55&q=${searchedQuery}&per_page=9&image_type=photo&orientation=horizontal&safesearch=true`)
-      
-        .then(response => {
+  const searchParams = new URLSearchParams({
+    key: '48282241-c94e9d668c7a92092d53abf55',
+    q: searchedQuery,
+    per_page: '9',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
 
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-      
-            return response.json();
-        });
-      
+  return axios.get(`https://pixabay.com/api/?${searchParams}`);
+
+  //   return fetch(`https://pixabay.com/api/?${searchParams}`).then(response => {
+  //     if (!response.ok) {
+  //       throw new Error(response.status);
+  //     }
+
+  //     return response.json();
+  //   });
 };
